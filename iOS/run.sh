5009a43b2ead004ede2acbffb6387041fbedb2a9
Eric Lanz <ericandrewlanz@gmail.com>
2020-12-10 16:11:13 -0800

    update to 1.0.2

--- iOS/run.sh
+++ iOS/run.sh
@@ -1,9 +1,64 @@
 #!/bin/bash
 
-# min search depth to use when looking for frameworks. expose this as an envvar to allow modification in an Xcode build phase.
-EMBRACE_DSYM_SEARCH_DEPTH=2
-# set this value to suppress warnings about needing to upload dSYMs manually when Bitcode is enabled
+# Embrace dSYM upload wrapper script
+#
+# Scans build output directories for dSYMs to upload
+#
+#
+# Required environment variables
+# ------------------------------
+#
+# BUILD_ROOT
+#
+# The path to the build root directory. This is an environment set by default by XCode. If you are using this script
+# outside of an XCode build phase, you must specify this manually.
+#
+# EMBRACE_ID
+#
+# The app ID that dSYMs are being uploaded for. This should be a 5-character value.
+#
+# EMBRACE_TOKEN
+#
+# The API token that authenticates the upload. This a 32-digit hex number.
+#
+#
+# Optional environment variables
+# ------------------------------
+#
+# DEBUG_INFORMATION_FORMAT
+#
+# This is an environment variable set by XCode. If it is set and is not "dwarf-with-dsym", the upload is skipped since
+# we won't have a dSYM for the app. If it is not set, we will scan for dSYMs. There is typically no need to set this
+# manually.
+#
+# DISABLE_BITCODE_CHECK (default=<not set>)
+#
+# A warning is printed if you run this script with Bitcode support enabled since the dSYMs created during the build
+# process are largely useless and you will have to download dSYMs from Apple instead. Set this variable to suppress this
+# warning.
+#
+# EMBRACE_DEBUG_DSYM (default=<not set>)
+#
+# Set this to force log output from the upload binary to appear in the XCode build logs. When this is not set, stderr
+# and stdout are redirected to /dev/null and the logs are written to the system log to allow the upload to occur in the
+# background. When debug mode is enabled, the upload does no longer happens in the background, which will add time to
+# the build process.
+#
+# EMBRACE_FRAMEWORK_SEARCH_DEPTH (default=2)
+#
+# Search depth to start at relative to the framework search path when scanning for framework dSYMs. Most configurations
+# do not require this value to be set.
+#
+# EMBRACE_FRAMEWORK_SEARCH_PATH (default=<not set>)
+#
+# Use this to specify a custom path to search for framework dSYMs. Most configurations do not require this value to be
+# set.
+
+
 DISABLE_BITCODE_CHECK=
+EMBRACE_FRAMEWORK_SEARCH_DEPTH=
+
+DEFAULT_FRAMEWORK_SEARCH_DEPTH=2
 
 [ -n "$CONFIGURATION_BUILD_DIR" -a -n "$UNLOCALIZED_RESOURCES_FOLDER_PATH" ] && REACT_NATIVE_BUNDLE_PATH_DEFAULT="$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH/main.jsbundle"
 
@@ -136,6 +191,7 @@ function upload() {
     done
 
     if [ -n "$found" ] ; then
+        log "Queueing app dSYM for upload: $app_dsym_file"
         icon=$(echo "$PWD"/*/*.xcassets/AppIcon.appiconset/Icon-60@2x.png | head -n 1)
         if [ -n "$icon" ] ; then
           icon_arg="--icon $icon"
@@ -148,14 +204,38 @@ function upload() {
     fi
 
     # use `find` and a for loop to build up the list of dSYMs rather than a glob so we can properly handle escaping of spaces
-    framework_dsyms=$(find . -mindepth $EMBRACE_DSYM_SEARCH_DEPTH -name "*.dSYM")
+    framework_search_path=$PWD
+    framework_search_depth=$DEFAULT_FRAMEWORK_SEARCH_DEPTH
+
+    if [ -n "$EMBRACE_FRAMEWORK_SEARCH_PATH" ] ; then
+      if [ -e "$EMBRACE_FRAMEWORK_SEARCH_PATH" ] ; then
+        framework_search_path=$EMBRACE_FRAMEWORK_SEARCH_PATH
+        log "Using custom framework search path set with EMBRACE_FRAMEWORK_SEARCH_PATH"
+      else
+        log_error "Specified custom framework search path $EMBRACE_FRAMEWORK_SEARCH_PATH, but it does not exist. Using default ${framework_search_path}."
+      fi
+    elif [ -n "$DWARF_DSYM_FOLDER_PATH" ] ; then
+      framework_search_path=$DWARF_DSYM_FOLDER_PATH
+      log "Using DWARF_DSYM_FOLDER_PATH to set framework search path"
+    elif [ -n "$BUILT_PRODUCTS_DIR" ] ; then
+      framework_search_path=$BUILT_PRODUCTS_DIR
+      log "Using BUILT_PRODUCTS_DIR to set framework search path"
+    fi
+
+    if [ -n "$EMBRACE_FRAMEWORK_SEARCH_DEPTH" ] ; then
+      framework_search_depth=$EMBRACE_FRAMEWORK_SEARCH_DEPTH
+      log "Overriding default framework search depth and setting it to $framework_search_depth"
+    fi
+
+    log "Scanning for framework dSYMs in $framework_search_path"
+    framework_dsyms=$(find "$framework_search_path" -mindepth $framework_search_depth -name "*.dSYM")
     if [ -n "$framework_dsyms" ] ; then
         ORIG_IFS=$IFS
         IFS=$'\n'
         dsym_args=""
         for dsym in $framework_dsyms ; do
             # we already uploaded the main dsym above, skip it here
-            if [[ $dsym == *$DWARF_DSYM_FILE_NAME ]] ; then
+            if [[ -n $DWARF_DSYM_FILE_NAME && $dsym == *$DWARF_DSYM_FILE_NAME ]] ; then
                 continue
             fi
             dsym_full_path="$(find "${dsym}"/Contents/Resources/DWARF/*  -type f 2>/dev/null)"
@@ -163,6 +243,7 @@ function upload() {
                 continue
             fi
             dsym_args="$dsym_args --dsym \"$dsym_full_path\""
+            log "Queueing framework dSYM for upload: $dsym_full_path"
         done
         IFS=$ORIG_IFS
         run "\"$DIR\"/upload --app $EMBRACE_ID --token $EMBRACE_TOKEN $dsym_args"
@@ -170,14 +251,14 @@ function upload() {
 }
 
 
-run_msg="Running"
 if [ -n "$EMBRACE_DEBUG_DSYM" ] ; then
-    run_msg="$run_msg in debug mode"
+    log "Debug mode enabled"
     unset EMBRACE_USE_SYSLOG
 else
     export EMBRACE_USE_SYSLOG=1
+    log "Logs from the upload binary will appear in the system log. In macOS you can find them with 'grep embrace /var/log/system.log'"
+    log "Set EMBRACE_DEBUG_DSYM=1 to display them in the XCode build logs."
 fi
-log "$run_msg"
 
 if [ "$ENABLE_BITCODE" = "YES" -a -z "$DISABLE_BITCODE_CHECK" ] ; then
     log "\"Enable bitcode\" is set to \"YES\". You will need to download dSYMs from Apple for TestFlight and App Store builds and then upload those dSYMs to Embrace."
